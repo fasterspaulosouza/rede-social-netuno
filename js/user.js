24bf38aa280c2da0d7f4e014ade6108f9fd5ac0d
Paulo Souza <paulos@portasabertas.org.br>
2025-09-04 18:09:33 -0300

    update projeto

--- js/user.js
+++ js/user.js
@@ -5,17 +5,33 @@ document.addEventListener("DOMContentLoaded", function () {
   // Convertendo de volta para objeto
   const usuarioObj = JSON.parse(usuarioSalvo);
 
-  console.log(usuarioObj);
-
   document.querySelector("#imagemPerfil").src = usuarioObj.foto;
   document.querySelector("#nomePerfil").innerText = usuarioObj.nome;
   document.querySelector("#emailPerfil").innerText = usuarioObj.email;
 
+  // Loggoff
   const btnSair = document.querySelector("#btnSair");
-
   btnSair.addEventListener("click", () => {
     localStorage.removeItem("usuario");
     alert("Usuário foi deslogado com sucesso!");
     window.location.href = "/index.html";
   });
+
+  // Chamada API para carregar nossas publicaçoes:
+
+  const requestOptions = {
+    method: "GET",
+  };
+
+  fetch("http://localhost:3000/publicacoes", requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      document.querySelector(".imgPerfilPub").src = result[0].foto;
+      document.querySelector(".nomePerfilPub").innerText = result[0].nome;
+      document.querySelector(".dataPerfilPub").innerText = result[0].dtcadastro;
+      document.querySelector(".imgPub").src = result[0].imagem;
+      document.querySelector(".textoPub").innerText = result[0].texto;
+      document.querySelector(".urlPub").href = result[0].url;
+    })
+    .catch((error) => console.error(error));
 });
