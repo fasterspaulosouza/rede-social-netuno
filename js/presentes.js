09d4ae3b86ff751d61b4a1c7cbf6ddd8bb1a9db4
Paulo Souza <paulos@portasabertas.org.br>
2025-09-11 18:49:08 -0300

    criacao pagina de presentes

---
+++ js/presentes.js
@@ -0,0 +1,46 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Recuperando o item usuário
+  const usuarioSalvo = localStorage.getItem("usuario");
+
+  // Convertendo de volta para objeto
+  const usuarioObj = JSON.parse(usuarioSalvo);
+
+  // Loggoff
+  const btnSair = document.querySelector("#btnSair");
+  btnSair.addEventListener("click", () => {
+    localStorage.removeItem("usuario");
+    alert("Usuário foi deslogado com sucesso!");
+    window.location.href = "/index.html";
+  });
+
+  // GET AMIGOS
+  const caixaAmigos = document.getElementById("amigos");
+
+  const requestOptions = {
+    method: "GET",
+  };
+
+  fetch("http://localhost:3000/amigos", requestOptions)
+    .then((response) => response.json())
+    .then((result) => {
+      caixaAmigos.innerHTML = "";
+      let conteudo = "";
+
+      result.forEach((amg) => {
+        conteudo = `
+          <div class="amigo">
+            <div class="boxImagemAmigo">
+              <img src="${amg.foto}" alt="Foto da ${amg.nome}" />
+            </div>
+            <div class="boxInfoAmigo">
+              <p class="nomeAmigo">${amg.nome}</p>
+              <p class="emailAmigo">${amg.email}</p>
+            </div>
+          </div>
+        `;
+
+        caixaAmigos.insertAdjacentHTML("beforeEnd", conteudo);
+      });
+    })
+    .catch((error) => console.error(error));
+});
